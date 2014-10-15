8488541402b3c76eeb3e71789b68fac34f792fe3
bryancort <bryancort@gmail.com>
2014-10-15 14:58:24 -0400

    jobs.py dev commit

--- mri_automation/jobs.py
+++ mri_automation/jobs.py
@@ -32,32 +32,24 @@ class MriJob:
 
 
 class TransferJob(MriJob):
-    def __init__(self):
-        MriJob.__init__(self)
 
     def run(self):
         pass
 
 
 class ProcessingJob(MriJob):
-    def __init__(self):
-        MriJob.__init__(self)
 
     def run(self):
         pass
 
 
 class ConversionJob(MriJob):
-    def __init__(self):
-        MriJob.__init__(self)
 
     def run(self):
         pass
 
 
 class MultiJob(MriJob):
-    def __init__(self, ):
-        MriJob.__init__(self)
 
     def run(self):
         pass
