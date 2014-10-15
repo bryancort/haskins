5d1558e7e29ed2c1baed3a0f87ac051352ccf379
bryancort <bryancort@gmail.com>
2014-10-15 11:34:38 -0400

    jobs.py dev commit

--- mri_automation/jobs.py
+++ mri_automation/jobs.py
@@ -8,14 +8,27 @@
 # -------------------------------------------------------------------------------
 
 
+import multiprocessing as mp
 
 
 class MriJob:
-    def __init__(self, lock=None):
+    def __init__(self, worker, exec_func, lock=None, blocking=False, worker_func=mp.apply_async, ):
+        """
+
+        :param worker: worker thread or pool to execute the job. Must implement apply() and apply_async()
+        :param exec_func: function to be executed by worker
+        :param lock: optional lock to acquire before executing the job
+        :param blocking: blocking parameter for lock
+        :param worker_func: function for worker to execute the job with
+        """
+        self.worker = worker
+        self.exec_func = exec_func
         self.lock = lock
+        self.blocking = blocking
+        self.worker_func = worker_func
 
     def run(self):
-        pass
+        raise NotImplementedError('Method not implemented in parent')
 
 
 class TransferJob(MriJob):
