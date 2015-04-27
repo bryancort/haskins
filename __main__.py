b4417aa36941aa33a1e8edd09561ca458aeae06c
bryancort <bryancort@gmail.com>
2015-04-27 15:42:59 -0400

    commit

---
+++ __main__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+from task_scripts.mri_stats import gen_mvm
+
+gen_mvm.__main__()
--- experiments/__init__.py
+++
@@ -1,8 +0,0 @@
-# -------------------------------------------------------------------------------
-# Name:        experiments
-# Purpose:      psychopy implementations of haskins experimental protocols
-#
-# Author:      Bryan Cort
-#
-# Created:     12/11/2014
-# -------------------------------------------------------------------------------
