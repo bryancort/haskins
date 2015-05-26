e204d355719f07dc388fdc79ed172e37917f71da
bryancort <bryancort@gmail.com>
2015-05-26 15:39:16 -0400

    commit

--- task_scripts/mri_stats/gen_mvm_sample_inputs/sample_call.sh
+++ task_scripts/mri_stats/gen_mvm_sample_inputs/sample_call.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
-python ../gen_mvm.py ARGS
+python ../gen_mvm.py \
+--mri_dir sample_mri_subjects \
+--within_vars_spec_file sample_within.txt \
+--between_vars_spec_file sample_between.txt \
+--body_entry_spec_file sample_body.txt \
+--output_dir gen_mvm_sample_outputs \
+--proc_run .scale \
+--output_table_name sample_mvm_table.txt \
+--output_call_name sample_mvm_call.sh \
+--quant_covars dummy_covar1,dummy_covar2 \
+--quant_covars_centers 20,30 \
+--vox_covar SFNR \
+--vox_covar_pattern *SFNR*.HEAD
