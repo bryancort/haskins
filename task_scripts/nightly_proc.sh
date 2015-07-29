48d9d4a925db7cc58da876df8e07feebacdce59b
bryancort <bryancort@gmail.com>
2015-07-29 17:15:19 -0400

    commit

--- task_scripts/nightly_proc.sh
+++ task_scripts/nightly_proc.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+# Define a timestamp function
+timestamp() {
+  date +"%m_%d_%y"
+}
+
+# do something...
+ts=`timestamp` # print timestamp
+
 . /Users/cort/.bashrc
-python /data1/scripts_refactor/haskins/task_scripts/a182_brain_nii_copy.py
+python /data1/scripts_refactor/haskins/task_scripts/a182_brain_nii_copy.py 2>&1 | tee /data1/logs/log_${ts}
